c41608cdb2fa2e1ddd920a38cb0e883847ab404e
sm-nat <141369048+sm-nat@users.noreply.github.com>
2023-10-12 17:13:07 +0000

    casi casi

--- exercises/12-Add-Options-to-the-Select/index.js
+++ exercises/12-Add-Options-to-the-Select/index.js
@@ -1,11 +1,20 @@
 let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
-// your code here
 
-let mySelect = document.getElementById("#mySelect");
-mySelect.appendChild("countries");
+let listSize = countries.length;
+
+let mySelect = document.getElementById("mySelect");
+
+for (let i = 0; i < listSize; i++) {
+    let option = document.createElement('option');
+    option.value = countries[i];
+    option.textContent = countries[i];
+
+    mySelect.appendChild(option);
+}
+
+mySelect.addEventListener('change', (e) => {
+    alert("Selected Country: " + e.target.value);
+});
 
 
-mySelect.addEventListener("change", () => {
-    alert("Selected Country: " + mySelect.value);
-  });
 
--- exercises/13-Todo-List/index.js
+++ exercises/13-Todo-List/index.js
@@ -1,1 +1,45 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+  const inputTodo = document.getElementById("addToDo");
+  const ulTodoList = document.querySelector("ul");
+
+  const createTodoElement = (task) => {
+      const li = document.createElement("li");
+      li.textContent = task;
+
+      li.addEventListener("click", () => {
+          li.classList.toggle("clicked");
+      });
+
+      const deleteButton = document.createElement("span");
+      deleteButton.innerHTML = '.span';
+      deleteButton.addEventListener("click", () => {
+          span.remove();
+      });
+
+      span.appendChild(deleteButton);
+
+      return li;
+  };
+  const addTaskButton = document.getElementById("addTaskButton");
+  addTaskButton.addEventListener("click", () => {
+      if (inputTodo.value.trim() !== "") {
+          const newTodo = createTodoElement(inputTodo.value.trim());
+          ulTodoList.appendChild(newTodo);
+          inputTodo.value = "";
+      }
+  });
+});
+
+  inputTodo.addEventListener("keypress", (event) => {
+      if (event.key === "Enter" && inputTodo.value.trim() !== "") {
+          const newTodo = createTodoElement(inputTodo.value.trim());
+          ulTodoList.appendChild(newTodo);
+          inputTodo.value = "";
+      }
+  });
+
+  
+
+
+  
+  
